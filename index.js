899ed36cdc17e0e5d144c412a40170c0e4ed0954
Slime_hatena <git@aki-memo.net>
2021-03-20 01:44:31 +0900

    fix: require was missing

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const core = require('@actions/core');
+const exec = require('@actions/exec');
 const github = require('@actions/github');
 const Output = require('./module/Output');
 const SemanticVersion = require('./module/SemanticVersion');
